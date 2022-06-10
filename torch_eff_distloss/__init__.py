0c2daf5a6618fa336d145daafe3ec45fd3859686
Cheng Sun <s2821d3721@gmail.com>
2022-06-11 00:12:34 +0800

    implement torch_eff_distloss

---
+++ torch_eff_distloss/__init__.py
@@ -0,0 +1,6 @@
+from .eff_distloss import eff_distloss
+from .eff_distloss import eff_distloss_native
+from .eff_distloss import flatten_eff_distloss
+
+__all__ = ['eff_distloss', 'eff_distloss_native', 'flatten_eff_distloss']
+
